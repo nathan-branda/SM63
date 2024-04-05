30372345270f8eeae146f7fb6a30600bce2c9b2e
Nathan Branda <nab7450@g.rit.edu>
2024-04-05 11:19:03 -0400

    functions and beta-user input

--- scrapper.py
+++ scrapper.py
@@ -1,29 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
+import re
 
-
-url = 'https://www.speedrun.com/sm63?h=Any-Normal&x=7kj99423-68kojzn2.5leo4mqo'
+#User input of speedrun.com url
+def input_url() -> str:
+    while(True):
+        url = input("speedrun.com url(click on the category that you want):")
+        if(re.search("^speedrun.com")):
+            return url
+        else:
+            print("Not a speedrun.com URL")
 
 #Parse the html
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+def html_parse(url: str) -> str:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
 
-#Extract desired data
-player_names = []
-run_times = []
+#Allows user input for desired values
+def desired_values():
+    player_names = []
+    run_times = []
 
-table = soup.find_all('table')[0]
-world_titles = table.find_all('th')
-world_table_titles=[title.text.strip() for title in world_titles]
-print(world_table_titles)
-data_table = [world_table_titles]
+def main():
+    url=input_url()
+    soup=html_parse(url)
+    table = soup.find_all('table')[0]
+    world_titles = table.find_all('th')
+    world_table_titles=[title.text.strip() for title in world_titles]
+    print(world_table_titles)
+    data_table = [world_table_titles]
+    column_data = table.find_all('tr')
+    for row in column_data[2:]:
+        row_data=row.find_all('td')
+        individual_row_data = [data.text.strip() for data in row_data]
+        print(individual_row_data)
+        data_table.append(individual_row_data)
+    print(data_table)
 
-column_data = table.find_all('tr')
-for row in column_data[2:]:
-    row_data=row.find_all('td')
-    individual_row_data = [data.text.strip() for data in row_data]
-    print(individual_row_data)
-    data_table.append(individual_row_data)
-print(data_table)
+if __name__=="__main__":
+    main()
     
